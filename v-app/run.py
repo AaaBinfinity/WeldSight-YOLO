daa8e0ef180b5e8d4272ad87af55e36019c004c4
Binfinity <binfinity@qq.com>
2026-07-03 23:09:39 +0800

    maccom

--- v-app/run.py
+++ v-app/run.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     # 开发环境配置： threaded=True支持多线程（处理摄像头+API并发）
     app.run(
         host='0.0.0.0',
-        port=5000,
+        port=5098,
         threaded=True,
         debug=False  # 生产环境必须关闭debug
     )
